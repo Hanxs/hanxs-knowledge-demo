3104afe15e1b27031ae295b1184c2855fbc12baf
hanxs <hanxs@hanxsdeMacBook-Pro.local>
2026-09-12 19:18:06 +0800

    test

---
+++ src/main/java/com/example/knowledge/KnowlledgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.knowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowlledgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowlledgeApplication.class, args);
+	}
+
+}
